931e4c01264cf965d690fcee32d0d242ef919b1f
Danya <83755673+NoroMedafe@users.noreply.github.com>
2022-04-07 16:18:21 +0300

    Update Program.cs

--- shopCrystal/Program.cs
+++ shopCrystal/Program.cs
@@ -11,19 +11,19 @@ namespace shopCrystal
         static void Main(string[] args)
         {
             int userGold = 0;
-            int userCrystal= 0;
+            int userCrystals= 0;
             int costCrystal = 2;
             int purchasedСrystals;
            
             Console.Write("Сколько у вас золота? "); userGold = Convert.ToInt32(Console.ReadLine());
             while (userGold > 0)
             {
-                Console.Write("Сколько кристаллов хотите купить? Цена кристала " + costCrystal + " золотых: "); purchasedСrystals = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Сколько кристаллов хотите купить?" +"\n"+ "Цена кристала " + costCrystal + " золотых: "); purchasedСrystals = Convert.ToInt32(Console.ReadLine());
 
                 userGold -= purchasedСrystals * costCrystal;
-                userCrystal += purchasedСrystals;
+                userCrystals += purchasedСrystals;
 
-                Console.WriteLine("Ваш баланс:" + "\n" + "Осталось золота:" + userGold + " Имеется кристалов:" + userCrystal);
+                Console.WriteLine("Ваш баланс:" + "\n" + "Осталось золота:" + userGold + " Имеется кристалов:" + userCrystals);
                 Console.ReadKey();
                 Console.Clear();
             }
